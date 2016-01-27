28b2687e88d0e0fd5ab0e3b1c196f4ceb5d1774c
SandroBritto <sandro.britto@epitech.eu>
2016-01-27 20:04:24 +0100

    Correction

---
+++ app/src/main/java/britto/sandro/epiandroid/LoginActivity.java
@@ -0,0 +1,124 @@
+package britto.sandro.epiandroid;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends AppCompatActivity {
+
+    @ViewById
+    AutoCompleteTextView vlogin;
+
+    @ViewById
+    EditText vpassword;
+
+    @UiThread
+    void settingFieldError() {
+        vlogin.setError(null);
+        vpassword.setError(null);
+    }
+
+    @UiThread
+    void setLoginRequired() {
+        vlogin.setError(getString(R.string.error_field_required));
+    }
+
+    @UiThread
+    void setWrongLoginRequired() {
+        vlogin.setError(getString(R.string.error_login_required));
+    }
+
+    @UiThread
+    void setPasswdRequired() {
+        vpassword.setError(getString(R.string.error_field_required));
+    }
+
+    private boolean isLoginValid(String Login) {
+        if (Login.length() > 8 || Login.indexOf('_') == -1)
+            return (false);
+        return (true);
+    }
+
+    @UiThread
+    void setView(View focusView) {
+        focusView.requestFocus();
+    }
+
+    @Background
+    void attemptLogin() {
+        String login = vlogin.getText().toString();
+        String passwd = vpassword.getText().toString();
+
+        View focusView = null;
+
+        boolean cancel = false;
+
+        settingFieldError();
+
+        if (TextUtils.isEmpty(passwd)) {
+            setPasswdRequired();
+            focusView = vpassword;
+            cancel = true;
+        }
+        if (TextUtils.isEmpty(login)) {
+            setLoginRequired();
+            focusView = vlogin;
+            cancel = true;
+        } else if (!isLoginValid(login)) {
+            setWrongLoginRequired();
+            focusView = vlogin;
+            cancel = true;
+        }
+        if (cancel)
+            setView(focusView);
+        else
+            startActivity(new Intent(this, MainActivity_.class));
+        }
+
+    @Click(R.id.login_sign_in_button)
+    void SignInClicked() {
+         attemptLogin();
+        }
+}
---
+++ app/src/main/java/britto/sandro/epiandroid/ModuleActivity.java
@@ -0,0 +1,73 @@
+package britto.sandro.epiandroid;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_module)
+public class ModuleActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @ViewById
+    Toolbar toolbar;
+
+    @ViewById
+    DrawerLayout drawer_layout;
+
+    @ViewById
+    NavigationView nav_view;
+
+    @AfterViews
+    void init() {
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer_layout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        nav_view.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile) {
+            drawer_layout.closeDrawer(GravityCompat.START);
+            startActivity(new Intent(this, MainActivity_.class));
+        } else if (id == R.id.nav_planning) {
+            drawer_layout.closeDrawer(GravityCompat.START);
+            startActivity(new Intent(this, PlanningActivity_.class));
+        } else if (id == R.id.nav_module) {
+            drawer_layout.closeDrawer(GravityCompat.START);
+            startActivity(new Intent(this, ModuleActivity_.class));
+        } else if (id == R.id.nav_mark) {
+
+        } else if (id == R.id.nav_logout) {
+            drawer_layout.closeDrawer(GravityCompat.START);
+            startActivity(new Intent(this, LoginActivity_.class));
+        }
+        drawer_layout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
+
